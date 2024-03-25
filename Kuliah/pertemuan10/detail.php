42a105dcfc8fbdca524f6ca8fc51ad4d8103a743
Ramdani1997 <ramdaniquraisshihab09@gmail.com>
2024-03-25 23:35:39 +0700

    Koneksi Database

---
+++ Kuliah/pertemuan10/detail.php
@@ -0,0 +1,34 @@
+<?php
+require 'functions.php';
+
+// ambil id dari url
+
+$id = $_GET['id'];
+
+//query mahasiswa by id
+$m = query("select * from mahasiswa where id = $id");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>"></li>
+    <li>NIM :<?= $m['nim']; ?></li>
+    <li>Nama : <?= $m['nama']; ?></li>
+    <li>Email : <?= $m['email']; ?></li>
+    <li>Jurusan : <?= $m['jurusan']; ?></li>
+    <li><a href="">Ubah</a> | <a href="">Hapus</a></li>
+    <li><a href="latihan3.php">Kembali ke Daftar Mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
---
+++ Kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,60 @@
+<?php
+// Koneksi ke DB & Pilih Databse
+$conn = mysqli_connect('localhost', 'root', '', 'pw_20240325');
+
+
+// Query isi Table Mahasiswa
+$result = mysqli_query($conn, "select * from mahasiswa");
+
+
+// Ubah data ke dalam array
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+
+
+$mahasiswa = $rows;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpaddiing="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NIM</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
+        <td><?= $m['nim']; ?></td>
+        <td><?= $m['nama']; ?></td>
+        <td><?= $m['email']; ?></td>
+        <td><?= $m['jurusan']; ?></td>
+        <td>
+          <a href="">Ubah</a> | <a href="">Hapus</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
